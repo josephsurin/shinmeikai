573c5e0bbbc61e81b63cdaaa8dfd005f301499c9
fumetsuu <joseph.surin@gmail.com>
2019-03-17 11:23:12 +1100

    discord integration

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
+import discord
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 from src.dict import init_dict, dict_search
 import time
+import re
 
-def main():
-        smk_dict = init_dict()
-        while True:
-                cmdargs = input('command:  ')
-                cmd = cmdargs.split()[0]
-                if cmd == 'exit':
-                        break
-                if cmd in ['s', 'search']:
+smk_dict = init_dict()
+
+class ShinmeikaiClient(discord.Client):
+        async def on_ready(self):
+                print('新明解 started', self.user)
+
+        async def on_message(self, message):
+                if message.author == self.user:
+                        return
+
+                if re.match(r'^(!s|ｓ|ｓ)', message.content): 
+                        search_q = message.content.split()[1]
+
                         start = time.time()
-                        search_q = cmdargs.split()[1]
                         matches = dict_search(search_q, smk_dict)
-                        print(matches)
                         end = time.time()
                         dur = end - start
                         print(f'found {len(matches)} words in {dur} seconds')
+                        await message.channel.send(str(matches)[:2000])
 
-
-if __name__ == '__main__':
-        main()
+client = ShinmeikaiClient()
+client.run(os.getenv('TOKEN'))
